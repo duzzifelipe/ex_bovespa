17d73e83750dcf260149600bac43ec7e04506091
Felipe Eduardo Duzzi <duzzifelipe@gmail.com>
2020-08-21 17:39:59 -0300

    chore: new mix project

---
+++ lib/ex_bovespa.ex
@@ -0,0 +1,18 @@
+defmodule ExBovespa do
+  @moduledoc """
+  Documentation for `ExBovespa`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExBovespa.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_bovespa_test.exs
@@ -0,0 +1,8 @@
+defmodule ExBovespaTest do
+  use ExUnit.Case
+  doctest ExBovespa
+
+  test "greets the world" do
+    assert ExBovespa.hello() == :world
+  end
+end
