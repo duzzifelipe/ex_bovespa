aad8dae1c7be4ddd756b17c5699801d2c8dadb4b
Felipe Eduardo Duzzi <duzzifelipe@gmail.com>
2020-08-22 14:33:50 -0300

    feat: list retrieval module

--- config/config.exs
+++ config/config.exs
@@ -2,4 +2,8 @@ use Mix.Config
 
 if Mix.env() == :test do
   config :tesla, adapter: Tesla.Mock
+
+  config :ex_bovespa,
+    parallelism: 1,
+    adapter_module: ExBovespa.Adapters.BovespaMock
 end
--- lib/ex_bovespa.ex
+++ lib/ex_bovespa.ex
@@ -4,10 +4,68 @@ defmodule ExBovespa do
   bovespa website by webscraping their HTML data
   """
 
+  alias ExBovespa.Parsers.{StockDetailHtml, StockListHtml}
+  alias ExBovespa.Structs.Stock
+
+  require Logger
+
+  @parallel_chunk_size Application.get_env(:ex_bovespa, :parallelism, 100)
+  @adapter_module Application.get_env(:ex_bovespa, :adapter_module, ExBovespa.Adapters.Bovespa)
+
   @doc """
-  Returns a list of all stocks from bovespa website
+  Returns a list of all stocks from bovespa website.
+
+  While running tests on development environment,
+  it took more than 1min 30s to load the entire
+  list of currently availble stocks on B3.
+
+  ### Example
+
+      iex> ExBovespa.stock_list()
+      [
+        %Stock{}
+      ]
   """
-  @spec stock_list() :: nil
+  @spec stock_list() :: {:ok, list(Stock.t())} | {:error, :invalid_response}
   def stock_list do
+    Logger.debug("#{__MODULE__}.stock_list")
+
+    case @adapter_module.get_list() do
+      {:ok, html} ->
+        {:ok, parse_items(html)}
+
+      error ->
+        Logger.error("#{__MODULE__}.stock_list error=#{inspect(error)}")
+        error
+    end
+  end
+
+  defp parse_items(html) do
+    html
+    |> StockListHtml.parse()
+    |> Enum.chunk_every(@parallel_chunk_size)
+    |> Enum.map(&get_items/1)
+    |> List.flatten()
+  end
+
+  defp get_items(stock_list) do
+    Logger.debug("#{__MODULE__}.get_items")
+
+    stock_list
+    |> Enum.map(&Task.async(fn -> get_item(&1) end))
+    |> Enum.map(&Task.await(&1, :infinity))
+  end
+
+  defp get_item(%Stock{company_code: code} = stock) do
+    Logger.debug("#{__MODULE__}.get_item")
+
+    case @adapter_module.get_item(code) do
+      {:ok, html} ->
+        StockDetailHtml.parse(html, stock)
+
+      error ->
+        Logger.error("#{__MODULE__}.get_item error=#{inspect(error)}")
+        stock
+    end
   end
 end
