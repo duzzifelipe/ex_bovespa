ac0f9bb6c3e32353c325a343af3adc3f6e36c9a8
Felipe Eduardo Duzzi <duzzifelipe@gmail.com>
2020-08-22 11:16:29 -0300

    chore: add html parser library

--- mix.exs
+++ mix.exs
@@ -40,6 +40,7 @@ defmodule ExBovespa.MixProject do
       {:credo, "~> 1.3.2", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.0.0", runtime: false, allow_pre: false, only: [:dev, :test]},
       {:ex_doc, "~> 0.22", only: :dev, runtime: false},
+      {:floki, "~> 0.27.0"},
       {:tesla, "~> 1.3.0"}
     ]
   end
