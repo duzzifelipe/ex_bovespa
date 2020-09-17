7a86de989f1b36dad8e040e3803c2ea546a64e62
Felipe Eduardo Duzzi <duzzifelipe@gmail.com>
2020-09-17 09:02:52 -0300

    feat: status invest client

---
+++ lib/adapters/status_invest_behaviour.ex
@@ -0,0 +1,12 @@
+defmodule ExBovespa.Adapters.StatusInvestBehaviour do
+  @moduledoc """
+  Defines the functions implemented by Status Invest adapter
+  """
+
+  @callback get_fii_list() :: {:ok, map()} | {:error, :invalid_response}
+
+  @callback get_stock_list() :: {:ok, map()} | {:error, :invalid_response}
+
+  @callback get_stock_price(code :: String.t(), type :: String.t()) ::
+              {:ok, map()} | {:error, :invalid_response}
+end
