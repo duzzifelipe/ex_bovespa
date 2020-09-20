4a6c5a050806f482ba411e5ec11adbd923c3a13a
Felipe Eduardo Duzzi <duzzifelipe@gmail.com>
2020-09-20 20:30:56 -0300

    feat: txt file parser

---
+++ lib/parsers/price_row_csv.ex
@@ -0,0 +1,89 @@
+defmodule ExBovespa.Parsers.PriceRowCsv do
+  @moduledoc """
+  From a text file name (as string), breaks
+  each line and decode each column by their
+  fixed string size based on the layout
+  provided by B3 (at section "What is the file layout?"):
+  http://www.b3.com.br/en_us/market-data-and-indices/data-services/market-data/historical-data/equities/historical-quote-data/
+  """
+
+  def parse(file_name) do
+    [header | items] =
+      file_name
+      |> File.stream!()
+      |> Stream.with_index()
+      |> Stream.map(&decode_line/1)
+      |> Enum.to_list()
+
+    [
+      header: header,
+      items: items
+    ]
+  end
+
+  defp decode_line({line, 0}), do: decode_header(line)
+
+  defp decode_line({line, _index}), do: decode_item(line)
+
+  defp decode_header(
+         <<_type::binary-size(2), file_name::binary-size(13), source::binary-size(8),
+           created_at::binary-size(8), _::bitstring>>
+       ) do
+    %{
+      file_name: file_name,
+      source: String.trim(source),
+      created_at: parse_date(created_at)
+    }
+  end
+
+  defp decode_item(
+         <<_tipreg::binary-size(2), date::binary-size(8), codbdi::binary-size(2),
+           codneg::binary-size(12), tpmerc::binary-size(3), nomres::binary-size(12),
+           especi::binary-size(10), prazot::binary-size(3), modref::binary-size(4),
+           preabe::binary-size(13), premax::binary-size(13), premin::binary-size(13),
+           premed::binary-size(13), preult::binary-size(13), preofc::binary-size(13),
+           preofv::binary-size(13), totneg::binary-size(5), quatot::binary-size(18),
+           voltot::binary-size(18), preexe::binary-size(13), indopc::binary-size(1),
+           datven::binary-size(8), fatcot::binary-size(7), ptoexe::binary-size(13),
+           codisi::binary-size(12), dismes::binary-size(3), _::bitstring>>
+       ) do
+    %{
+      date: parse_date(date),
+      bdi: codbdi,
+      code: String.trim(codneg),
+      isin_code: codisi,
+      market_type: tpmerc,
+      company_name: String.trim(nomres),
+      specification: String.trim(especi),
+      market_term: String.trim(prazot),
+      currency_symbol: String.trim(modref),
+      opening_price: parse_float(preabe),
+      closing_price: parse_float(preult),
+      lowest_price: parse_float(premin),
+      highest_price: parse_float(premax),
+      average_price: parse_float(premed),
+      best_purchase_price: parse_float(preofc),
+      best_sell_price: parse_float(preofv),
+      total_trades: String.to_integer(totneg),
+      titles_traded: String.to_integer(quatot),
+      volume_traded: parse_float(voltot),
+      strike_price: parse_float(preexe),
+      strike_price_correction: indopc,
+      maturity_date: parse_date(datven),
+      quotation_factor: String.to_integer(fatcot),
+      strike_price_points: String.to_integer(ptoexe),
+      distribution_number: dismes
+    }
+  end
+
+  defp parse_date(string) do
+    string
+    |> Timex.parse!("{YYYY}{M}{D}")
+    |> NaiveDateTime.to_date()
+  end
+
+  defp parse_float(string) do
+    integer = String.to_integer(string)
+    Decimal.new(1, integer, -2)
+  end
+end
---
+++ test/parsers/price_row_csv_test.exs
@@ -0,0 +1,90 @@
+defmodule ExBovespa.Parsers.PriceRowCsvTest do
+  use ExUnit.Case
+  alias ExBovespa.Parsers.PriceRowCsv
+
+  setup do
+    template = """
+    00COTAHIST.2020BOVESPA 20200918
+    012020091802A1AP34      010ADVANCE AUTODRN ED       R$  000000002059800000000205980000000020598000000002059800000000205980000000000000000000002216000001000000000000003710000000000076418580000000000000009999123100000010000000000000BRA1APBDR001103
+    012020091802A1BM34      010ABIOMED INC DRN          R$  000000003541100000000354110000000035411000000003541100000000354110000000000000000000000000000001000000000000000070000000000002478770000000000000009999123100000010000000000000BRA1BMBDR006100
+    """
+
+    file_name = "/tmp/#{Base.url_encode64(:crypto.strong_rand_bytes(16))}.txt"
+    File.write!(file_name, template)
+
+    [file_name: file_name]
+  end
+
+  describe "parse/1" do
+    test "should parse headers", %{file_name: file_name} do
+      assert [header: header, items: _] = PriceRowCsv.parse(file_name)
+
+      assert header == %{
+               file_name: "COTAHIST.2020",
+               source: "BOVESPA",
+               created_at: ~D[2020-09-18]
+             }
+    end
+
+    test "should parse lines", %{file_name: file_name} do
+      assert [header: _, items: items] = PriceRowCsv.parse(file_name)
+
+      assert items == [
+               %{
+                 date: ~D[2020-09-18],
+                 bdi: "02",
+                 code: "A1AP34",
+                 market_type: "010",
+                 company_name: "ADVANCE AUTO",
+                 specification: "DRN ED",
+                 currency_symbol: "R$",
+                 market_term: "",
+                 average_price: Decimal.from_float(205.98),
+                 best_purchase_price: elem(Decimal.parse("0.00"), 1),
+                 best_sell_price: elem(Decimal.parse("221.60"), 1),
+                 closing_price: Decimal.from_float(205.98),
+                 distribution_number: "103",
+                 highest_price: Decimal.from_float(205.98),
+                 isin_code: "BRA1APBDR001",
+                 lowest_price: Decimal.from_float(205.98),
+                 maturity_date: ~D[9999-12-31],
+                 opening_price: Decimal.from_float(205.98),
+                 quotation_factor: 1,
+                 strike_price: elem(Decimal.parse("0.00"), 1),
+                 strike_price_correction: "0",
+                 strike_price_points: 0,
+                 titles_traded: 3710,
+                 total_trades: 1,
+                 volume_traded: elem(Decimal.parse("764185.80"), 1)
+               },
+               %{
+                 date: ~D[2020-09-18],
+                 bdi: "02",
+                 code: "A1BM34",
+                 market_type: "010",
+                 company_name: "ABIOMED INC",
+                 specification: "DRN",
+                 currency_symbol: "R$",
+                 market_term: "",
+                 average_price: Decimal.from_float(354.11),
+                 best_purchase_price: elem(Decimal.parse("0.00"), 1),
+                 best_sell_price: elem(Decimal.parse("0.00"), 1),
+                 closing_price: Decimal.from_float(354.11),
+                 distribution_number: "100",
+                 highest_price: Decimal.from_float(354.11),
+                 isin_code: "BRA1BMBDR006",
+                 lowest_price: Decimal.from_float(354.11),
+                 maturity_date: ~D[9999-12-31],
+                 opening_price: Decimal.from_float(354.11),
+                 quotation_factor: 1,
+                 strike_price: elem(Decimal.parse("0.00"), 1),
+                 strike_price_correction: "0",
+                 strike_price_points: 0,
+                 titles_traded: 70,
+                 total_trades: 1,
+                 volume_traded: elem(Decimal.parse("24787.70"), 1)
+               }
+             ]
+    end
+  end
+end
