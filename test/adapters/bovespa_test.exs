b29faf57b3c60ef69081cdfb4b736e2773a2c5c8
Felipe Eduardo Duzzi <duzzifelipe@gmail.com>
2020-09-21 19:52:44 -0300

    test: validation for adapter

--- test/adapters/bovespa_test.exs
+++ test/adapters/bovespa_test.exs
@@ -5,16 +5,18 @@ defmodule ExBovespa.Adapters.BovespaTest do
 
   alias ExBovespa.Adapters.Bovespa
 
-  @base_url "http://bvmf.bmfbovespa.com.br/cias-listadas/Titulos-Negociaveis"
+  @base_url "http://bvmf.bmfbovespa.com.br"
 
-  @list_url @base_url <> "/ResumoTitulosNegociaveis.aspx"
+  @list_url @base_url <> "/cias-listadas/Titulos-Negociaveis/ResumoTitulosNegociaveis.aspx"
   @list_query [or: "bus", tip: "N", cb: "", idioma: "pt-BR"]
 
-  @details_url @base_url <> "/DetalheTitulosNegociaveis.aspx"
+  @details_url @base_url <> "/cias-listadas/Titulos-Negociaveis/DetalheTitulosNegociaveis.aspx"
   @details_query [or: "res", tip: "N", cb: "", idioma: "pt-BR"]
 
+  @price_url @base_url <> "/InstDados/SerHist"
+
   describe "get_list/0" do
-    test "should return the html content if status is 200" do
+    test "should return the html content for yearly file if status is 200" do
       mock(fn %{method: :get, url: @list_url, query: @list_query} ->
         %Tesla.Env{status: 200, body: "<html></html>"}
       end)
@@ -56,6 +58,96 @@ defmodule ExBovespa.Adapters.BovespaTest do
         end)
 
         assert {:error, :invalid_response} = Bovespa.get_item("IBOV")
+      end
+    end
+  end
+
+  describe "get_historical_file/1" do
+    test "should return binary content for monthly file if status is 200" do
+      year = to_string(Enum.random(1970..2050))
+      url = @price_url <> "/COTAHIST_A#{year}.ZIP"
+
+      mock(fn %{method: :get, url: ^url} ->
+        %Tesla.Env{status: 200, body: "data"}
+      end)
+
+      assert {:ok, "data"} = Bovespa.get_historical_file(year)
+    end
+
+    test "should return error for other statuses" do
+      statuses = Enum.to_list(100..199) ++ Enum.to_list(201..599)
+
+      for status <- statuses do
+        year = to_string(Enum.random(1970..2050))
+        url = @price_url <> "/COTAHIST_A#{year}.ZIP"
+
+        mock(fn %{method: :get, url: ^url} ->
+          %Tesla.Env{status: status, body: ""}
+        end)
+
+        assert {:error, :invalid_response} = Bovespa.get_historical_file(year)
+      end
+    end
+  end
+
+  describe "get_historical_file/2" do
+    test "should return binary content if status is 200" do
+      year = to_string(Enum.random(1970..2050))
+      month = to_string(Enum.random(1..12))
+      url = @price_url <> "/COTAHIST_M#{month}#{year}.ZIP"
+
+      mock(fn %{method: :get, url: ^url} ->
+        %Tesla.Env{status: 200, body: "data"}
+      end)
+
+      assert {:ok, "data"} = Bovespa.get_historical_file(year, month)
+    end
+
+    test "should return error for other statuses" do
+      statuses = Enum.to_list(100..199) ++ Enum.to_list(201..599)
+
+      for status <- statuses do
+        year = to_string(Enum.random(1970..2050))
+        month = to_string(Enum.random(1..12))
+        url = @price_url <> "/COTAHIST_M#{month}#{year}.ZIP"
+
+        mock(fn %{method: :get, url: ^url} ->
+          %Tesla.Env{status: status, body: ""}
+        end)
+
+        assert {:error, :invalid_response} = Bovespa.get_historical_file(year, month)
+      end
+    end
+  end
+
+  describe "get_historical_file/3" do
+    test "should return binary content for daily file if status is 200" do
+      year = to_string(Enum.random(1970..2050))
+      month = to_string(Enum.random(1..12))
+      day = to_string(Enum.random(1..31))
+      url = @price_url <> "/COTAHIST_D#{day}#{month}#{year}.ZIP"
+
+      mock(fn %{method: :get, url: ^url} ->
+        %Tesla.Env{status: 200, body: "data"}
+      end)
+
+      assert {:ok, "data"} = Bovespa.get_historical_file(year, month, day)
+    end
+
+    test "should return error for other statuses" do
+      statuses = Enum.to_list(100..199) ++ Enum.to_list(201..599)
+
+      for status <- statuses do
+        year = to_string(Enum.random(1970..2050))
+        month = to_string(Enum.random(1..12))
+        day = to_string(Enum.random(1..31))
+        url = @price_url <> "/COTAHIST_D#{day}#{month}#{year}.ZIP"
+
+        mock(fn %{method: :get, url: ^url} ->
+          %Tesla.Env{status: status, body: ""}
+        end)
+
+        assert {:error, :invalid_response} = Bovespa.get_historical_file(year, month, day)
       end
     end
   end
