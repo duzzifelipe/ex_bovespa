75c864ce8c4cfbd0e265c9c34779b06dd5bfa67e
Felipe Eduardo Duzzi <duzzifelipe@gmail.com>
2020-09-21 20:13:11 -0300

    test: service caller test

--- test/ex_bovespa_test.exs
+++ test/ex_bovespa_test.exs
@@ -4,7 +4,7 @@ defmodule ExBovespaTest do
   import ExUnit.CaptureLog
   import Mox
 
-  alias ExBovespa.Structs.{Broker, Stock, StockDetail}
+  alias ExBovespa.Structs.{Broker, PriceRowHeader, PriceRowItem, Stock, StockDetail}
 
   defp list_content_mock do
     """
@@ -29,6 +29,25 @@ defmodule ExBovespaTest do
       </body>
     </html>
     """
+  end
+
+  defp price_file_mock do
+    file_name = "/tmp/#{Base.encode16(:crypto.strong_rand_bytes(16), padding: false)}"
+    file_name_char = String.to_charlist(file_name)
+
+    file_content = """
+    00COTAHIST.2020BOVESPA 20200918
+    012020091802A1AP34      010ADVANCE AUTODRN ED       R$  000000002059800000000205980000000020598000000002059800000000205980000000000000000000002216000001000000000000003710000000000076418580000000000000009999123100000010000000000000BRA1APBDR001103
+    012020091802A1BM34      010ABIOMED INC DRN          R$  000000003541100000000354110000000035411000000003541100000000354110000000000000000000000000000001000000000000000070000000000002478770000000000000009999123100000010000000000000BRA1BMBDR006100
+    99COTAHIST.2020BOVESPA 2020091800000006212
+    """
+
+    :ok = File.write!(file_name <> ".txt", file_content)
+
+    {:ok, zip_dir} =
+      :zip.create(file_name_char ++ '.zip', [file_name_char ++ '.txt'], cwd: file_name_char)
+
+    File.read!(zip_dir)
   end
 
   describe "stock_list/0" do
@@ -206,6 +225,142 @@ defmodule ExBovespaTest do
       end)
 
       assert {:error, :invalid_response} = ExBovespa.broker_list()
+    end
+  end
+
+  describe "historical_price/1" do
+    @sample_result [
+      header: %PriceRowHeader{
+        file_name: "COTAHIST.2020",
+        source: "BOVESPA",
+        created_at: ~D[2020-09-18]
+      },
+      items: [
+        %PriceRowItem{
+          date: ~D[2020-09-18],
+          bdi: "02",
+          code: "A1AP34",
+          market_type: "010",
+          company_name: "ADVANCE AUTO",
+          specification: "DRN ED",
+          currency_symbol: "R$",
+          market_term: "",
+          average_price: Decimal.from_float(205.98),
+          best_purchase_price: elem(Decimal.parse("0.00"), 1),
+          best_sell_price: elem(Decimal.parse("221.60"), 1),
+          closing_price: Decimal.from_float(205.98),
+          distribution_number: "103",
+          highest_price: Decimal.from_float(205.98),
+          isin_code: "BRA1APBDR001",
+          lowest_price: Decimal.from_float(205.98),
+          maturity_date: ~D[9999-12-31],
+          opening_price: Decimal.from_float(205.98),
+          quotation_factor: 1,
+          strike_price: elem(Decimal.parse("0.00"), 1),
+          strike_price_correction: "0",
+          strike_price_points: 0,
+          titles_traded: 3710,
+          total_trades: 1,
+          volume_traded: elem(Decimal.parse("764185.80"), 1)
+        },
+        %PriceRowItem{
+          date: ~D[2020-09-18],
+          bdi: "02",
+          code: "A1BM34",
+          market_type: "010",
+          company_name: "ABIOMED INC",
+          specification: "DRN",
+          currency_symbol: "R$",
+          market_term: "",
+          average_price: Decimal.from_float(354.11),
+          best_purchase_price: elem(Decimal.parse("0.00"), 1),
+          best_sell_price: elem(Decimal.parse("0.00"), 1),
+          closing_price: Decimal.from_float(354.11),
+          distribution_number: "100",
+          highest_price: Decimal.from_float(354.11),
+          isin_code: "BRA1BMBDR006",
+          lowest_price: Decimal.from_float(354.11),
+          maturity_date: ~D[9999-12-31],
+          opening_price: Decimal.from_float(354.11),
+          quotation_factor: 1,
+          strike_price: elem(Decimal.parse("0.00"), 1),
+          strike_price_correction: "0",
+          strike_price_points: 0,
+          titles_traded: 70,
+          total_trades: 1,
+          volume_traded: elem(Decimal.parse("24787.70"), 1)
+        }
+      ]
+    ]
+
+    test "returns list items" do
+      year = to_string(Enum.random(1970..2050))
+
+      expect(ExBovespa.Adapters.BovespaMock, :get_historical_file, fn ^year ->
+        {:ok, price_file_mock()}
+      end)
+
+      assert {:ok, @sample_result} == ExBovespa.historical_price(year)
+    end
+
+    test "returns http error from list service" do
+      year = to_string(Enum.random(1970..2050))
+
+      expect(ExBovespa.Adapters.BovespaMock, :get_historical_file, fn ^year ->
+        {:error, :invalid_response}
+      end)
+
+      assert {:error, :invalid_response} = ExBovespa.historical_price(year)
+    end
+  end
+
+  describe "historical_price/2" do
+    test "returns list items" do
+      year = to_string(Enum.random(1970..2050))
+      month = to_string(Enum.random(1..12))
+
+      expect(ExBovespa.Adapters.BovespaMock, :get_historical_file, fn ^year, ^month ->
+        {:ok, price_file_mock()}
+      end)
+
+      assert {:ok, @sample_result} == ExBovespa.historical_price(year, month)
+    end
+
+    test "returns http error from list service" do
+      year = to_string(Enum.random(1970..2050))
+      month = to_string(Enum.random(1..12))
+
+      expect(ExBovespa.Adapters.BovespaMock, :get_historical_file, fn ^year, ^month ->
+        {:error, :invalid_response}
+      end)
+
+      assert {:error, :invalid_response} = ExBovespa.historical_price(year, month)
+    end
+  end
+
+  describe "historical_price/3" do
+    test "returns list items" do
+      year = to_string(Enum.random(1970..2050))
+      month = to_string(Enum.random(1..12))
+      day = to_string(Enum.random(1..31))
+
+      expect(ExBovespa.Adapters.BovespaMock, :get_historical_file, fn ^year, ^month, ^day ->
+        {:ok, price_file_mock()}
+      end)
+
+      assert {:ok, @sample_result} == ExBovespa.historical_price(year, month, day)
+    end
+
+    test "returns http error from list service" do
+      year = to_string(Enum.random(1970..2050))
+      month = to_string(Enum.random(1..12))
+      day = to_string(Enum.random(1..31))
+
+      expect(ExBovespa.Adapters.BovespaMock, :get_historical_file, fn ^year, ^month, ^day ->
+        {:error, :invalid_response}
+      end)
+
+      assert {:error, :invalid_response} = ExBovespa.historical_price(year, month, day)
     end
   end
 end
