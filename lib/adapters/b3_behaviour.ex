d7915ab7f933e764ec47ac9565a9283da572da1b
Felipe Eduardo Duzzi <duzzifelipe@gmail.com>
2020-12-05 14:25:01 -0300

    refactor: use rest service for company list

--- lib/adapters/b3_behaviour.ex
+++ lib/adapters/b3_behaviour.ex
@@ -3,6 +3,6 @@ defmodule ExBovespa.Adapters.B3Behaviour do
   Defines the functions implemented by B3 adapter
   """
 
-  @callback get_company_list_by_page(page :: pos_integer()) ::
-              {:ok, String.t()} | {:error, :invalid_response}
+  @callback get_company_list ::
+              {:ok, list(map())} | {:error, :invalid_response}
 end
